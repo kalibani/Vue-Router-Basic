b5d3b852c4829e975a58f9648fba22ae2684a8a4
kalibani <kalibani.ka@gmail.com>
2017-11-19 19:21:30 +0700

    simple Single Page App

---
+++ router-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-link :to="{ name: 'HelloWorld' }">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/coins/ethereum">Ethereum</router-link>
+    <router-link to="/coins/bitcoin">Bitcoin</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
